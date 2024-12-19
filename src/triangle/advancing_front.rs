0770a0b1582f172573fce4c3ee09322c3635515a
XerusN <134720540+XerusN@users.noreply.github.com>
2024-12-19 12:38:37 +0100

    remove clippy warnings by commenting some lines

--- src/triangle/advancing_front.rs
+++ src/triangle/advancing_front.rs
@@ -1,8 +1,8 @@
-use cfd_rs_utils::*;
+//use cfd_rs_utils::*;
 
 // /// Advancing front algorithm.
 // /// The boundaries need to be locked, unexpected things might happen if some edges are shared between boundaries.
 // /// Same if some boundaries are defined the wrong way (clockwise or not defines the direction of expansion)
-pub fn advancing_front(boundaries: &Vec<Boundary2D>, edges: Vec<Edge2D>, nodes: Vec<Point2<f64>>) -> MeshBlock2D<Triangle> {
+pub fn advancing_front() {
     todo!()
 }
