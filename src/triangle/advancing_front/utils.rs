6e5b78f0e366bffb2f841d9c7363040457c24472
XerusN <134720540+XerusN@users.noreply.github.com>
2025-02-26 12:08:46 +0100

    edge intersect triangle

--- src/triangle/advancing_front/utils.rs
+++ src/triangle/advancing_front/utils.rs
@@ -88,7 +88,33 @@ pub fn edge_intersect_triangle(triangle: &[Point2<f64>], edge: &[Point2<f64>]) -
         }
     }
 
-    false
+    // 2. If one or two of the points is in the triangle => intersection
+    if point_in_triangle(triangle, &edge[0]) {
+        return true;
+    }
+    if point_in_triangle(triangle, &edge[1]) {
+        return true;
+    }
+
+    // 3. Intersection directly (rare)
+    let mut t0: f64 = 0.;
+    let mut t1: f64 = 1.;
+    for i in 0..3 {
+        let b0 = half_plane(&[triangle[i], triangle[(i + 1) % 3]], &edge[0]);
+        let b1 = half_plane(&[triangle[i], triangle[(i + 1) % 3]], &edge[1]);
+        if b0 * b1 < 0. {
+            if b0 < 0. {
+                t0 = t0.max(b0 / (b0 - b1));
+            } else {
+                t1 = t1.min(b0 / (b0 - b1));
+            }
+            if t0 > t1 {
+                return false;
+            }
+        }
+    }
+
+    true
 }
 
 /// Determines if a point is on the positive or negative half-plane of an edge
@@ -99,14 +125,12 @@ pub fn half_plane(edge: &[Point2<f64>], point: &Point2<f64>) -> f64 {
 
 /// Tells if a point is in a triangle, the triangle points must be in the trigonometric order.
 pub fn point_in_triangle(triangle: &[Point2<f64>], point: &Point2<f64>) -> bool {
-    
     // A point is in a triangle if and only if he is on the 3 positive half-planes
     for i in 0..3 {
-        if half_plane(&[triangle[i], triangle[(i + 1) % 3]], point) <= 0.
-        {
-            return false
+        if half_plane(&[triangle[i], triangle[(i + 1) % 3]], point) <= 0. {
+            return false;
         }
     }
-    
-    return true
+
+    return true;
 }
